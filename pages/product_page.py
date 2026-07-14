a27564637699b86f4c155d8ecf315c8218d0695f
Alex <stikhinsky@gmail.com>
2026-07-14 21:50:49 +0300

    add changes to: product_page.py, test_product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,19 +1,29 @@
 from .locators import ProductPageLocators
 from .base_page import BasePage
 
+
 class ProductPage(BasePage):
     def add_to_basket(self):
         basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket_button.click()
         self.solve_quiz_and_get_code()
 
+    def get_book_name(self):
+        return self.browser.find_element(*ProductPageLocators.BOOKNAME_IN_CARD).text
+
+    def get_book_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRICE_IN_CARD).text
+
     def should_be_basket_button(self):
         # проверка на наличие кнопки "Добавить в корзину"
         assert self.browser.find_element(*ProductPageLocators.BASKET_BUTTON), "Кнопка 'Добавить в корзину' отсутствует"
 
-    def should_be_same_bookname(self):
+    def should_be_same_bookname(self, book_name_in_card: str):
         # проверка на совпадение имени добавленной книги с именем в информационном сообщении, после добавления в корзину
-        assert self.browser.find_element(*ProductPageLocators.BOOKNAME_IN_CARD).text == self.browser.find_element(*ProductPageLocators.BOOKNAME_IN_MESSAGE).text, "Название книги на карточке и в информационном сообщении не совпадают"
-    def should_be_same_bookprice(self):
+        assert book_name_in_card == self.browser.find_element(
+            *ProductPageLocators.BOOKNAME_IN_MESSAGE).text, "Название книги на карточке и в информационном сообщении не совпадают"
+
+    def should_be_same_bookprice(self, book_price_in_card: str):
         # проверка на совпадение цены добавленной книги с ценой в информационном сообщении, после добавления в корзину
-        assert self.browser.find_element(*ProductPageLocators.PRICE_IN_CARD).text == self.browser.find_element(*ProductPageLocators.PRICE_IN_MESSAGE).text, "Цена книги на карточке и в информационном сообщении не совпадают"
+        assert book_price_in_card == self.browser.find_element(
+            *ProductPageLocators.PRICE_IN_MESSAGE).text, "Цена книги на карточке и в информационном сообщении не совпадают"
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,25 @@
+import pytest
+
 from pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser,
+                       link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.should_be_basket_button()
+    book_name = page.get_book_name()
+    book_price = page.get_book_price()
     page.add_to_basket()  # добавляем товар в корзину
-    page.should_be_same_bookname()
-    page.should_be_same_bookprice()
+    page.should_be_same_bookname(book_name)
+    page.should_be_same_bookprice(book_price)
